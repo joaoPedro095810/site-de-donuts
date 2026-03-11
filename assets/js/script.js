12f3150ab30f32b56d85e08d120f58fd60a50233
joaoPedro095810 <joao.ferreira483@etec.psp.gov.br>
2026-03-10 22:13:39 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+console.log("Site de Donuts carregado 🍩");
+
+const botaoExplorar = document.querySelector(".primary");
+
+botaoExplorar.addEventListener("click", () => {
+
+alert("Explorando nossos donuts deliciosos 🍩");
+
+});
+// botões da index
+const btnExplorar = document.getElementById("explorar")
+const btnSabores = document.getElementById("verSabores")
+
+// botão da página sabores
+const btnCardapio = document.getElementById("verCardapio")
+
+// botão para voltar
+const btnInicio = document.getElementById("voltarInicio")
+
+// index → sabores
+if(btnSabores){
+btnSabores.addEventListener("click", () => {
+window.location.href = "sabores.html"
+})
+}
+
+// index → cardapio
+if(btnExplorar){
+btnExplorar.addEventListener("click", () => {
+window.location.href = "cardapio.html"
+})
+}
+
+// sabores → cardapio
+if(btnCardapio){
+btnCardapio.addEventListener("click", () => {
+window.location.href = "cardapio.html"
+})
+}
+
+// qualquer página → index
+if(btnInicio){
+btnInicio.addEventListener("click", () => {
+window.location.href = "index.html"
+})
+}
